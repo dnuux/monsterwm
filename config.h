119554894d4110ef295e975f7c18f9809680209f
dnuux <dnuuxx@gmail.com>
2012-09-02 22:14:59 +0300

    Personal configuration

--- config.h
+++ config.h
@@ -3,11 +3,6 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-/* "name" desktops */
-enum { CURRENT=-1, WEB, DEV, FOO, NIL };
-/* "name" monitors */
-enum { LARGE, SMALL };
-
 /** modifiers **/
 #define MOD1            Mod1Mask    /* ALT key */
 #define MOD4            Mod4Mask    /* Super/Windows key */
@@ -21,18 +16,17 @@ enum { LARGE, SMALL };
 #define PANEL_HEIGHT    18        /* 0 for no space for panel, thus no panel */
 #define DEFAULT_MODE    TILE      /* initial layout/mode: TILE MONOCLE BSTACK GRID FLOAT */
 #define ATTACH_ASIDE    True      /* False means new window is master */
-#define FOLLOW_WINDOW   True      /* follow the window when moved to a different desktop */
+#define FOLLOW_WINDOW   False     /* follow the window when moved to a different desktop */
 #define FOLLOW_MONITOR  False     /* follow the window when moved to a different monitor */
 #define FOLLOW_MOUSE    False     /* focus the window the mouse just entered */
-#define CLICK_TO_FOCUS  False     /* focus an unfocused window when clicked  */
+#define CLICK_TO_FOCUS  True      /* focus an unfocused window when clicked  */
 #define BORDER_WIDTH    2         /* window border width */
 #define FOCUS           "#ff950e" /* focused window border color   */
 #define UNFOCUS         "#666666" /* unfocused window border color */
 #define INFOCUS         "#9c3885" /* focused window border color on unfocused monitor */
 #define MINWSZ          50        /* minimum window size in pixels */
 #define DESKTOPS        4         /* number of desktops - edit DESKTOPCHANGE keys to suit */
-#define DEFAULT_DESKTOP WEB       /* the desktop to focus initially */
-#define DEFAULT_MONITOR LARGE     /* the monitor to focus initially */
+#define DEFAULT_DESKTOP 1         /* the desktop to focus initially */
 
 struct ml {
     int m; /* monitor that the desktop in on  */
@@ -56,13 +50,11 @@ struct ml {
  *   the panel will be hidden.
  */
 static const struct ml init[] = { \
-    /* monitor  desktop   mode    masz  sbar   */
-    {   SMALL,    WEB,  { BSTACK, 400,  False } },
-    {   SMALL,    DEV,  { GRID,    0,   False } },
-    {   SMALL,    FOO,  { TILE,    0,   False } },
-    {   SMALL,    NIL,  { FLOAT,   0,   False } },
-    {   LARGE,    WEB,  { TILE,    0,   True  } },
-    {   LARGE,    DEV,  { GRID,    0,   False } },
+    /* monitor  desktop   mode  masz  sbar   */
+    {     0,       0,   { TILE, -180, True  } },
+    {     0,       1,   { TILE,  300, True  } },
+    {     0,       2,   { TILE, -150, True  } },
+    {     1,       0,   { TILE,    0, False } },
 };
 
 /**
@@ -73,101 +65,93 @@ static const struct ml init[] = { \
  */
 static const AppRule rules[] = { \
     /*  class     monitor  desktop  follow  float */
-    { "MPlayer",   SMALL,  CURRENT, True,   True  },
-    { "Gimp",      LARGE,    FOO,   True,   True  },
-    { "Deluge",    SMALL,    FOO,   False,  False },
-    { "IRC-",      LARGE,    WEB,   False,  False },
+    { "mplayer2",    0,      -1,    False,  True  },
+    { "sxiv",        0,      -1,    False,  True  },
+    { "Navigator",   0,       1,    False,  False },
+    { "Zathura",     0,       2,    False,  False },
+    { "rtorrent",    0,       3,    False,  False },
+    { "cmus",        0,       0,    False,  False },
+    { "htop",        0,       0,    False,  False },
+    { "vifm",        0,       0,    False,  False },
 };
-
-/* helper for spawning shell commands */
-#define SHCMD(cmd) {.com = (const char*[]){"/bin/sh", "-c", cmd, NULL}}
 
 /**
  * custom commands
  * must always end with ', NULL };'
  */
-static const char *termcmd[]  = { "xterm",     NULL };
-static const char *menucmd[]  = { "dmenu_run", NULL };
-static const char *torrent[]  = { "deluge",    NULL };
-static const char *ctermcmd[] = { "urxvtdc",   NULL };
-static const char *surfcmd[]  = { "chromium", "--enable-seccomp-sandbox", "--memory-model=low",
-                                  "--purge-memory-button", "--disk-cache-dir=/tmp/chromium", NULL };
+#define MENUFONT "-*-dejavu sans-medium-r-normal--10-*-*-*-*-*-iso8859-1"
+static const char *termcmd[] = { "urxvtc", NULL };
+static const char *menucmd[] = { "dmenu_run", "-i", "-fn", MENUFONT, NULL };
+static const char *filecmd[] = { "/home/elias/bin/flaunch", "file", "-i", "-fn", MENUFONT, NULL };
+static const char *dircmd[]  = { "/home/elias/bin/flaunch", "dir", "-i", "-fn", MENUFONT, NULL };
+static const char *warecmd[] = { "urxvtc", "-name", "rtorrent", "-e", "rtorrent", NULL };
+#undef MENUFONT
 
 /* audio volume */
-static const char *volupcmd[]     = { "volctrl", "+2",     NULL };
-static const char *voldncmd[]     = { "volctrl", "-2",     NULL };
-static const char *voltogglecmd[] = { "volctrl", "toggle", NULL };
-/* audio playback [mpd/mpc] */
-static const char *mstopcmd[]   = { "mpc", "stop",   NULL };
-static const char *mnextcmd[]   = { "mpc", "next",   NULL };
-static const char *mprevcmd[]   = { "mpc", "prev",   NULL };
-static const char *mtogglecmd[] = { "mpc", "toggle", NULL };
+static const char *voltogglecmd[]  = { "amixer", "-q", "sset", "Master", "toggle", NULL };
+static const char *voldowncmd[]    = { "amixer", "-q", "sset", "Master", "4-", "unmute", NULL };
+static const char *volupcmd[]      = { "amixer", "-q", "sset", "Master", "4+", "unmute", NULL };
+
+static const char *cmustogglecmd[] = { "cmus-remote", "-u", NULL };
+static const char *cmusprevcmd[]   = { "cmus-remote", "-r", NULL };
+static const char *cmusnextcmd[]   = { "cmus-remote", "-n", NULL };
 
 #define MONITORCHANGE(K,N) \
-    {  MOD4,             K,              change_monitor, {.i = N}}, \
-    {  MOD4|ShiftMask,   K,              client_to_monitor, {.i = N}},
+    {  MOD1,             K,              change_monitor, {.i = N}}, \
+    {  MOD1|ShiftMask,   K,              client_to_monitor, {.i = N}},
 
 #define DESKTOPCHANGE(K,N) \
-    {  MOD1,             K,              change_desktop, {.i = N}}, \
-    {  MOD1|ShiftMask,   K,              client_to_desktop, {.i = N}},
+    {  MOD4,             K,              change_desktop, {.i = N}}, \
+    {  MOD4|ShiftMask,   K,              client_to_desktop, {.i = N}},
 
 /**
  * keyboard shortcuts
  */
 static Key keys[] = {
     /* modifier          key            function           argument */
-    {  MOD1,             XK_b,          togglepanel,       {NULL}},
-    {  MOD1,             XK_BackSpace,  focusurgent,       {NULL}},
-    {  MOD1|SHIFT,       XK_c,          killclient,        {NULL}},
-    {  MOD1,             XK_j,          next_win,          {NULL}},
-    {  MOD1,             XK_k,          prev_win,          {NULL}},
-    {  MOD1,             XK_h,          resize_master,     {.i = -10}}, /* decrease size in px */
-    {  MOD1,             XK_l,          resize_master,     {.i = +10}}, /* increase size in px */
-    {  MOD1,             XK_o,          resize_stack,      {.i = -10}}, /* shrink   size in px */
-    {  MOD1,             XK_p,          resize_stack,      {.i = +10}}, /* grow     size in px */
-    {  MOD1|CONTROL,     XK_h,          rotate,            {.i = -1}},
-    {  MOD1|CONTROL,     XK_l,          rotate,            {.i = +1}},
-    {  MOD1|SHIFT,       XK_h,          rotate_filled,     {.i = -1}},
-    {  MOD1|SHIFT,       XK_l,          rotate_filled,     {.i = +1}},
-    {  MOD1,             XK_Tab,        last_desktop,      {NULL}},
-    {  MOD1,             XK_Return,     swap_master,       {NULL}},
-    {  MOD1|SHIFT,       XK_j,          move_down,         {NULL}},
-    {  MOD1|SHIFT,       XK_k,          move_up,           {NULL}},
-    {  MOD1|SHIFT,       XK_t,          switch_mode,       {.i = TILE}},
-    {  MOD1|SHIFT,       XK_m,          switch_mode,       {.i = MONOCLE}},
-    {  MOD1|SHIFT,       XK_b,          switch_mode,       {.i = BSTACK}},
-    {  MOD1|SHIFT,       XK_g,          switch_mode,       {.i = GRID}},
-    {  MOD1|SHIFT,       XK_f,          switch_mode,       {.i = FLOAT}},
-    {  MOD1|CONTROL,     XK_r,          quit,              {.i = 0}}, /* quit with exit value 0 */
-    {  MOD1|CONTROL,     XK_q,          quit,              {.i = 1}}, /* quit with exit value 1 */
-    {  MOD1|SHIFT,       XK_Return,     spawn,             {.com = termcmd}},
+    {  MOD4,             XK_j,          next_win,          {NULL}},
+    {  MOD4,             XK_k,          prev_win,          {NULL}},
+    {  MOD4|SHIFT,       XK_j,          move_down,         {NULL}},
+    {  MOD4|SHIFT,       XK_k,          move_up,           {NULL}},
+    {  MOD4|SHIFT,       XK_c,          killclient,        {NULL}},
+    {  MOD4,             XK_h,          resize_master,     {.i = -10}},
+    {  MOD4,             XK_l,          resize_master,     {.i = +10}},
+    {  MOD4,             XK_o,          resize_stack,      {.i = -20}},
+    {  MOD4,             XK_p,          resize_stack,      {.i = +20}},
+    {  0,                0x1008ff11,    spawn,             {.com = voldowncmd}},
+    {  0,                0x1008ff13,    spawn,             {.com = volupcmd}},
+    {  0,                0x1008ff12,    spawn,             {.com = voltogglecmd}},
+    {  0,                0x1008ff14,    spawn,             {.com = cmustogglecmd}},
+    {  0,                0x1008ff16,    spawn,             {.com = cmusprevcmd}},
+    {  0,                0x1008ff17,    spawn,             {.com = cmusnextcmd}},
+    {  MOD4|SHIFT,       XK_Return,     spawn,             {.com = termcmd}},
     {  MOD4,             XK_v,          spawn,             {.com = menucmd}},
-    {  MOD4,             XK_t,          spawn,             {.com = torrent}},
-    {  MOD4,             XK_w,          spawn,             {.com = surfcmd}},
-    {  MOD4,             XK_grave,      spawn,             {.com = ctermcmd}},
-    {  MOD4,             XK_equal,      spawn,             {.com = volupcmd}},
-    {  MOD4,             XK_KP_Add,     spawn,             {.com = volupcmd}},
-    {  MOD4,             XK_minus,      spawn,             {.com = voldncmd}},
-    {  MOD4,             XK_KP_Subtract,spawn,             {.com = voldncmd}},
-    {  MOD4,             XK_m,          spawn,             {.com = voltogglecmd}},
-    {  MOD4,             XK_s,          spawn,             {.com = mstopcmd}},
-    {  MOD4,             XK_period,     spawn,             {.com = mnextcmd}},
-    {  MOD4,             XK_comma,      spawn,             {.com = mprevcmd}},
-    {  MOD4,             XK_p,          spawn,             {.com = mtogglecmd}},
-    {  MOD4,             XK_j,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move up    */
-    {  MOD4,             XK_k,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move down  */
-    {  MOD4,             XK_l,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
-    {  MOD4,             XK_h,          moveresize,        {.v = (int []){ -25,   0,   0,   0 }}}, /* move left  */
-    {  MOD4|SHIFT,       XK_j,          moveresize,        {.v = (int []){   0,   0,   0,  25 }}}, /* height grow   */
-    {  MOD4|SHIFT,       XK_k,          moveresize,        {.v = (int []){   0,   0,   0, -25 }}}, /* height shrink */
-    {  MOD4|SHIFT,       XK_l,          moveresize,        {.v = (int []){   0,   0,  25,   0 }}}, /* width grow    */
-    {  MOD4|SHIFT,       XK_h,          moveresize,        {.v = (int []){   0,   0, -25,   0 }}}, /* width shrink  */
-       DESKTOPCHANGE(    XK_F1,                             WEB)
-       DESKTOPCHANGE(    XK_F2,                             DEV)
-       DESKTOPCHANGE(    XK_F3,                             FOO)
-       DESKTOPCHANGE(    XK_F4,                             NIL)
-       MONITORCHANGE(    XK_F1,                             SMALL)
-       MONITORCHANGE(    XK_F2,                             LARGE)
+    {  MOD4,             XK_f,          spawn,             {.com = filecmd}},
+    {  MOD4,             XK_d,          spawn,             {.com = dircmd}},
+    {  MOD4|SHIFT,       XK_r,          spawn,             {.com = warecmd}},
+    {  MOD4|SHIFT,       XK_t,          switch_mode,       {.i = TILE}},
+    {  MOD4|SHIFT,       XK_m,          switch_mode,       {.i = MONOCLE}},
+    {  MOD4|SHIFT,       XK_b,          switch_mode,       {.i = BSTACK}},
+    {  MOD4|SHIFT,       XK_g,          switch_mode,       {.i = GRID}},
+    {  MOD4|SHIFT,       XK_f,          switch_mode,       {.i = FLOAT}},
+    {  MOD4,             XK_Return,     swap_master,       {NULL}},
+    {  MOD4,             XK_b,          togglepanel,       {NULL}},
+    {  MOD4,             XK_Tab,        last_desktop,      {NULL}},
+    {  MOD4|CONTROL,     XK_q,          quit,              {NULL}},
+    {  MOD1,             XK_j,          moveresize,        {.v = (int []){   0,  25,   0,   0 }}}, /* move up    */
+    {  MOD1,             XK_k,          moveresize,        {.v = (int []){   0, -25,   0,   0 }}}, /* move down  */
+    {  MOD1,             XK_l,          moveresize,        {.v = (int []){  25,   0,   0,   0 }}}, /* move right */
+    {  MOD1,             XK_h,          moveresize,        {.v = (int []){ -25,   0,   0,   0 }}}, /* move left  */
+    {  MOD1|SHIFT,       XK_j,          moveresize,        {.v = (int []){   0,   0,   0,  25 }}}, /* height grow   */
+    {  MOD1|SHIFT,       XK_k,          moveresize,        {.v = (int []){   0,   0,   0, -25 }}}, /* height shrink */
+    {  MOD1|SHIFT,       XK_l,          moveresize,        {.v = (int []){   0,   0,  25,   0 }}}, /* width grow    */
+    {  MOD1|SHIFT,       XK_h,          moveresize,        {.v = (int []){   0,   0, -25,   0 }}}, /* width shrink  */
+       DESKTOPCHANGE(    XK_1,                              0)
+       DESKTOPCHANGE(    XK_2,                              1)
+       DESKTOPCHANGE(    XK_3,                              2)
+       DESKTOPCHANGE(    XK_4,                              3)
+       MONITORCHANGE(    XK_1,                              0)
+       MONITORCHANGE(    XK_2,                              1)
 };
 
 /**
@@ -176,6 +160,7 @@ static Key keys[] = {
 static Button buttons[] = {
     {  MOD1,    Button1,     mousemotion,   {.i = MOVE}},
     {  MOD1,    Button3,     mousemotion,   {.i = RESIZE}},
-    {  MOD4,    Button3,     spawn,         {.com = menucmd}},
 };
+
 #endif
+
